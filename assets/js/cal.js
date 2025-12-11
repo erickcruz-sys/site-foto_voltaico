ef16632a1abb8e4baec2f8ed49c42676b3c183ae
Erick <erick_cruz@estudante.sc.senai.br>
2025-12-11 18:28:37 -0300

    Versão inicial do site fotovoltaico

---
+++ assets/js/cal.js
@@ -0,0 +1,22 @@
+
+
+function calculo(kw, dias, efi, irradiacao) {
+return   kw * dias * efi * irradiacao / 1000
+}
+
+let result = document.getElementById('result')
+function main(){
+
+let kw = Number(document.getElementById('kw').value)
+let dias = Number(document.getElementById('dias').value)
+let efi = Number(document.getElementById('efi').value)
+let irradiacao = Number(document.getElementById('irradiacao').value)
+
+let placas = calculo(kw, dias, efi, irradiacao)
+console.log(placas)
+
+result.innerHTML = ``
+result.innerHTML += `A quantidade de placas ideal são: ${placas.toFixed(2)} kw`
+
+}
+
